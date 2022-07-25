e38bef5fe32d2010374d2e6ff2bf26ab9822d64e
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-07-25 16:49:38 +0300

    added trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const { data, isLoading } = trpc.useQuery(["hello", { text: "willbo" }]);
+  if (isLoading) return <div>Loading...</div>;
+  if (data) return <div>{data.greeting}</div>;
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center text-white">
       <div className="text-2xl text-center">Which Pokemon is rounder</div>
