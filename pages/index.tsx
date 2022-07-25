57f1cae610a1b2c24f5bfe8e1f09d6594f3fd019
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-07-25 14:57:15 +0300

    basic UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-red-600">
-      a whole new world
+    <div className="w-screen h-screen flex flex-col items-center justify-center text-white">
+      <div className="text-2xl text-center">Which Pokemon is rounder</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
     </div>
   );
 };
